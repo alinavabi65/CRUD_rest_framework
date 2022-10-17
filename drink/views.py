b0c1dfd6ba1d2b55e224a8808cdbe7cb6f10f727
Nasrin <a.navabi65@gmail.com>
2022-10-17 15:59:07 +0200

    impliment CRUD in django_rest_framework

---
+++ drink/views.py
@@ -0,0 +1,54 @@
+from rest_framework import status
+from rest_framework.response import Response
+
+from .serializers import DrinkSerializers
+from django.http import JsonResponse
+from .models import Drink
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST'])
+def drink_list(request, format=None):
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializers(drinks, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializers = DrinkSerializers(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id, format=None):
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializers(drink)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = DrinkSerializers(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+
+
